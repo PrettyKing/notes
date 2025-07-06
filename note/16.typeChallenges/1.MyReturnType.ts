a83e03938a4be013bcf019e560e2f74a846cc338
chalee <695469701@qq.com>
2025-07-06 21:14:41 +0800

    feat: U

--- note/16.typeChallenges/1.MyReturnType.ts
+++ note/16.typeChallenges/1.MyReturnType.ts
@@ -1,3 +1,11 @@
+const fn = (v:boolean) =>{
+    if(v)
+        return 1;
+    else
+        return 2;
+}
 
-
+// infer 只能在条件类型的 extends 子句中使用，用来声明一个类型变量，TypeScript 会自动推断这个变量的类型
 type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+
+type a = MyReturnType<typeof fn>;  // should be "1 | 2"
--- note/16.typeChallenges/2.Omit.ts
+++ note/16.typeChallenges/2.Omit.ts
@@ -1,4 +1,18 @@
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+}
 
+// Omit<T, K> 是 TypeScript 内置的类型工具，用于从类型 T 中排除属性 K
 type MyOmit<T, K> = {
     [P in keyof T as P extends K ? never : P]: T[P]
 }
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+
+const todo: TodoPreview = {
+    completed: false,
+}
+
+export {}
